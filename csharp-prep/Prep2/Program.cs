a086e0ba110408f3a11e0a103a37de3542d43876
becca217 <readwithbecca@gmail.com>
2024-01-13 21:53:13 -0700

    Created grade calculator

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int percentage = int.Parse(grade);
+        
+        string letter = "";
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"You have a {letter}.");
+        
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congrats! You are passing the class!");
+        }
+        else
+        {
+            Console.WriteLine("You are not currently passing the class, but you still have time! Work hard!");
+        }
     }
 }
