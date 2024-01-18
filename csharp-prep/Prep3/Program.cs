2a7997ed31857098b557e6c6be7971ada292fa40
becca217 <readwithbecca@gmail.com>
2024-01-17 22:34:48 -0700

    Guess number game + Random generator

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomNumber = new Random();
+        int randomMagicNumber = randomNumber.Next(1,100);
+
+        int userGuess = -1;
+
+        while (userGuess != randomMagicNumber)
+        {
+            Console.Write("What is your guess? ");
+            userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < randomMagicNumber)
+            {
+                Console.WriteLine("Higher.");
+            }
+            else if (userGuess > randomMagicNumber)
+            {
+                Console.WriteLine("Lower."); 
+            }
+            else
+            {
+                Console.WriteLine("You guessed the magic number!");
+            }
+        }
+
     }
 }
